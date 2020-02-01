6c3d22b52fd42adf374b0be2d43f9cb6704fb524
borossandor27 <boros.sanyi@gmail.com>
2020-02-01 21:26:40 +0100

    Add project files.

---
+++ Robot/Kodsorozat.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    class Kodsorozat
+    {
+        readonly string nev;
+        readonly string kod; //-- EJBH
+        int ivsz;   //-- irányváltások száma
+        int vegpontX = 0;
+        int vegpontY = 0;
+        double tavolsag;
+        bool hibas = false;
+
+        public string Nev => nev;
+
+        public string Kod => kod;
+
+        public int Ivsz { get => ivsz; set => ivsz = value; }
+        public int VegpontX { get => vegpontX; set => vegpontX = value; }
+        public int VegpontY { get => vegpontY; set => vegpontY = value; }
+        public double Tavolsag { get => tavolsag; set => tavolsag = value; }
+
+        public bool Hibas => hibas;
+
+        public Kodsorozat(string nev, string kod)
+        {
+            this.nev = nev;
+            this.kod = kod;
+            this.ivsz = Iranyvaltasok();
+            this.tavolsag = Math.Sqrt(Math.Pow(vegpontX, 2) + Math.Pow(vegpontY, 2));
+        }
+        int Iranyvaltasok()
+        {
+            int v = 0;
+            char elozo = kod[0];
+            foreach (char item in kod)
+            {
+                if (!item.Equals(elozo)) { v++; elozo = item; }
+                switch (item)
+                {
+                    case 'J':
+                        vegpontX++;
+                        break;
+                    case 'B':
+                        vegpontX--;
+                        break;
+                    case 'E':
+                        vegpontY++;
+                        break;
+                    case 'H':
+                        vegpontY--;
+                        break;
+                    default:
+                        hibas = true;
+                        break;
+                }
+            }
+            return v;
+        }
+    }
+}
---
+++ Robot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Robot
+{
+    class Program
+    {
+        static List<Kodsorozat> Kodok = new List<Kodsorozat>();
+        static void Main(string[] args)
+        {
+            ForrastBeolvas();
+            Console.WriteLine($"\n2. Feladat: Tanulók száma: {Kodok.Count} fő");
+            Console.WriteLine($"\n3. Feladat: Helytelen kódsorozatok száma: {Kodok.Where(a => a.Hibas).Count()}");
+            double max = Kodok.Where(a => !a.Hibas).Max(b => b.Tavolsag);
+            Console.WriteLine($"\n5. Feladat: Legtávolabbra jutó robot vezérlését készítette: {Kodok.Find(b => b.Tavolsag == max).Nev}");
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+
+        static void ForrastBeolvas()
+        {
+            string forras = @"..\..\progs.txt";
+            using (StreamReader sr = new StreamReader(forras))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split();
+                    Kodok.Add(new Kodsorozat(sor[0], sor[1]));
+                }
+            }
+        }
+    }
+}
